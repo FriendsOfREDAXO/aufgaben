ec3a2ef6d3e0fff979ebc9db4565eedfa11da58c
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2016-01-27 09:40:50 +0100

    Charset korrigiert

--- help.php
+++ help.php
@@ -1,9 +1,9 @@
-<h4>Eine ToDo Verwaltung für das Redaxo Backend.</h4>
+<h4>Eine ToDo Verwaltung fÃ¼r das Redaxo Backend.</h4>
 
 <ul>
-<li>im Beschreibungsfeld kann mit der Eingabe von ***** (5 Sterne) ein Trenner hinzugefügt werden.</li>
+<li>im Beschreibungsfeld kann mit der Eingabe von ***** (5 Sterne) ein Trenner hinzugefÃ¼gt werden.</li>
 <li>durch Klick auf "blaue" Aufgaben wird die Beschreibung angezeigt.</li>
-<li>durch Klick auf die Überschriften wird die Tabelle entsprechend sortiert.</li>
-<li>durch Klick auf das Auge (rechts im Tabellenkopf) können die erledigten Aufgaben dauerhaft ein- bzw. ausgeblendet werden.</li>
+<li>durch Klick auf die Ãœberschriften wird die Tabelle entsprechend sortiert.</li>
+<li>durch Klick auf das Auge (rechts im Tabellenkopf) kÃ¶nnen die erledigten Aufgaben dauerhaft ein- bzw. ausgeblendet werden.</li>
 <li>Kategorien werden durch den Admin gepflegt.</li>
 </ul>
