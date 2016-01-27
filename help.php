f7c1d9a5d655e2a659b39b1dc03d376193c114f7
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2016-01-27 09:38:51 +0100

    Tab 'Info' in Help-File verschoben

--- help.php
+++ help.php
@@ -1,3 +1,9 @@
-<b>Aufgaben AddOn</b>
+<h4>Eine ToDo Verwaltung für das Redaxo Backend.</h4>
 
-<br><br>
+<ul>
+<li>im Beschreibungsfeld kann mit der Eingabe von ***** (5 Sterne) ein Trenner hinzugefügt werden.</li>
+<li>durch Klick auf "blaue" Aufgaben wird die Beschreibung angezeigt.</li>
+<li>durch Klick auf die Überschriften wird die Tabelle entsprechend sortiert.</li>
+<li>durch Klick auf das Auge (rechts im Tabellenkopf) können die erledigten Aufgaben dauerhaft ein- bzw. ausgeblendet werden.</li>
+<li>Kategorien werden durch den Admin gepflegt.</li>
+</ul>
