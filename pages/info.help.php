654a4888491e600711ad49cd57798d3ced69f401
chrison94 <31507654+chrison94@users.noreply.github.com>
2017-09-11 10:15:10 +0200

    Update info.help.php

--- pages/info.help.php
+++ pages/info.help.php
@@ -15,11 +15,11 @@ if ($func == 'import_beispieldaten') {
   -- Aufgaben
 
   INSERT IGNORE `rex_aufgaben` VALUES
-      (1, 'Fav Icon erstellen', 'Wird immer benötigt',1,1,0,1,now(),now(),'$user','$user','',''),
-      (2, 'Touch Icon erstellen', '',1,1,0,1,now(),now(),'$user','$user','',''),
-      (3, 'Meta Infos erstellen', 'Sind Ortsbezogene meta Infos wichtig?',1,1,0,1,now(),now(),'$user','$user','',''),
-      (4, 'Print.css entwickeln', 'Wird immer vergessen',1,1,0,1,now(),now(),'$user','$user','',''),
-      (5, 'robots.txt prüfen', ':-)',7,1,0,1,now(),now(),'$user','$user','','');
+      (1, 'Fav Icon erstellen', 'Wird immer benötigt',1,1,0,1,now(),now(),'$user','$user','','','2'),
+      (2, 'Touch Icon erstellen', '',1,1,0,1,now(),now(),'$user','$user','','','1'),
+      (3, 'Meta Infos erstellen', 'Sind Ortsbezogene meta Infos wichtig?',1,1,0,1,now(),now(),'$user','$user','','','2'),
+      (4, 'Print.css entwickeln', 'Wird immer vergessen',1,1,0,1,now(),now(),'$user','$user','','','1'),
+      (5, 'robots.txt prüfen', ':-)',7,1,0,1,now(),now(),'$user','$user','','','2');
 
   -- Kategorien
 
