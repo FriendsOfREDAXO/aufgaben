a7260d738891e092e5560a0008445eee7d55bc03
Thomas Skerbis <thomas.skerbis@mac.com>
2019-02-15 17:44:32 +0100

    Umstellung auf rex_sql_table

--- install.php
+++ install.php
@@ -9,79 +9,53 @@ if (!$this->hasConfig()) {
   $this->setConfig('absender', "");
 }
 
-$sql = rex_sql::factory();
-$sql->setQuery('
-  CREATE TABLE IF NOT EXISTS `rex_aufgaben` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `title` varchar(255) DEFAULT NULL,
-    `description` longtext DEFAULT NULL,
-    `category` int(10) DEFAULT NULL,
-    `responsible` int(10) DEFAULT NULL,
-    `prio` int(10) DEFAULT NULL,
-    `status` int(10) DEFAULT NULL,
-    `createdate` DATETIME DEFAULT NULL,
-    `updatedate` DATETIME DEFAULT NULL,
-    `createuser` varchar(255) DEFAULT NULL,
-    `updateuser` varchar(255) DEFAULT NULL,
-    `observer` varchar(255) DEFAULT NULL,
-    `finaldate` varchar(255) DEFAULT NULL,
-    `versendet` varchar(255) DEFAULT "2",
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-');
+rex_sql_table::get(rex::getTable('aufgaben'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('title', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('description', 'longtext', true))
+    ->ensureColumn(new rex_sql_column('category', 'int(10)', true))
+    ->ensureColumn(new rex_sql_column('responsible', 'int(10)', true))
+    ->ensureColumn(new rex_sql_column('prio', 'int(10)', true))
+    ->ensureColumn(new rex_sql_column('status', 'int(10)', true))
+    ->ensureColumn(new rex_sql_column('createdate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('updatedate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('createuser', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('updateuser', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('observer', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('finaldate', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('versendet', 'varchar(255)', true, '2'))
+    ->ensure();
 
 
-$sql = rex_sql::factory();
-$sql->setQuery('CREATE TABLE IF NOT EXISTS `rex_aufgaben_categories` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `category` varchar(255) DEFAULT NULL,
-    `color` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-');
-
-$sql = rex_sql::factory();
-$sql->setQuery('CREATE TABLE IF NOT EXISTS `rex_aufgaben_status` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `status` varchar(255) DEFAULT NULL,
-    `icon` varchar(255)  DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-');
+rex_sql_table::get(rex::getTable('aufgaben_categories'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('category', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('color', 'varchar(255)', true))
+    ->ensure();
 
 
-$sql = rex_sql::factory();
-$sql->setQuery('CREATE TABLE IF NOT EXISTS `rex_aufgaben_filter` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `user` varchar(255) DEFAULT NULL,
-    `category` varchar(255)  DEFAULT NULL,
-    `responsible` varchar(255)  DEFAULT NULL,
-    `prio` varchar(255)  DEFAULT NULL,
-    `status` varchar(255) DEFAULT NULL,
-    `done` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-');
+rex_sql_table::get(rex::getTable('aufgaben_filter'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('user', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('category', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('responsible', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('prio', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('status', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('done', 'varchar(255)', true))
+    ->ensure();
 
-$sql = rex_sql::factory();
-$sql->setQuery('CREATE TABLE IF NOT EXISTS `rex_aufgaben_user_settings` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `user` int(10) DEFAULT NULL,
-    `counter` int(10)  DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-');
+rex_sql_table::get(rex::getTable('aufgaben_status'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('status', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('icon', 'varchar(255)', true))
+    ->ensure();
 
-// Sollte noch übersetzt werden -> wie?
-$sql = rex_sql::factory();
-$sql->setQuery("REPLACE INTO `rex_aufgaben_status` VALUES
-    (1,'Offen','fa-folder-open-o'),
-    (2,'Wird bearbeitet','fa-gears'),
-    (3,'Frage','fa-question'),
-    (4,'Warten auf etwas','fa-hourglass-start'),
-    (5,'Auf später verschoben','fa-calendar'),
-    (6,'Erledigt','fa-check');
-");
+rex_sql_table::get(rex::getTable('aufgaben_user_settings'))
+    ->ensurePrimaryIdColumn()
+    ->ensureColumn(new rex_sql_column('user', 'int(10)', true))
+    ->ensureColumn(new rex_sql_column('counter', 'int(10)', true))
+    ->ensure();
+
 
 $error = '';
 
